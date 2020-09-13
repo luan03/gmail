40a13b28c5ae84e8b3d8d073d47a3dc05a86fd01
Luan Fagundes <luan.fagundes@pebmed.com.br>
2020-09-13 19:30:47 -0300

    mail list

--- gmail/pages/index.js
+++ gmail/pages/index.js
@@ -8,13 +8,67 @@ export default function Home() {
       <Head>
         <title>Gmail</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet"></link>
       </Head>
 
       <header className={styles.header}>Search</header>
 
       <main className={styles.main}>
         <nav className={styles.navigation}>navigation menu</nav>
-        <aside className={styles.mails}>list of mail</aside>
+
+        <aside className={styles.mails}>
+          <ul>
+            <li>
+              <div className={styles.left}>
+                <span className={styles.checkbox}></span>
+                <span className={styles.avatar}><img src="uncle-bob.png" alt="Robert C. Martin" width="38" height="38" /></span>
+              </div>
+
+              <div className={styles.right}>
+                <span className={styles.name}>Uncle Bob <span className={styles.time}>15 mins</span></span>
+                <span className={styles.title}>Brazil.JS Conference 2020</span>
+                <span className={styles.preview}>Hey Luan, I'm confirming my presence in Brazil.JS conference 2020 and I'm hope to meet you soon...</span>
+              </div>
+            </li>
+            <li>
+              <div className={styles.left}>
+                <span className={styles.checkbox}></span>
+                <span className={styles.avatar}><img src="uncle-bob.png" alt="Robert C. Martin" width="38" height="38" /></span>
+              </div>
+
+              <div className={styles.right}>
+                <span className={styles.name}>Uncle Bob <span className={styles.time}>15 mins</span></span>
+                <span className={styles.title}>Brazil.JS Conference 2020</span>
+                <span className={styles.preview}>Hey Luan, I'm confirming my presence in Brazil.JS conference 2020 and I'm hope to meet you soon...</span>
+              </div>
+            </li>
+            <li>
+              <div className={styles.left}>
+                <span className={styles.checkbox}></span>
+                <span className={styles.avatar}><img src="uncle-bob.png" alt="Robert C. Martin" width="38" height="38" /></span>
+              </div>
+
+              <div className={styles.right}>
+                <span className={styles.name}>Uncle Bob <span className={styles.time}>15 mins</span></span>
+                <span className={styles.title}>Brazil.JS Conference 2020</span>
+                <span className={styles.preview}>Hey Luan, I'm confirming my presence in Brazil.JS conference 2020 and I'm hope to meet you soon...</span>
+              </div>
+            </li>
+            <li>
+              <div className={styles.left}>
+                <span className={styles.checkbox}></span>
+                <span className={styles.avatar}><img src="uncle-bob.png" alt="Robert C. Martin" width="38" height="38" /></span>
+              </div>
+
+              <div className={styles.right}>
+                <span className={styles.name}>Uncle Bob <span className={styles.time}>15 mins</span></span>
+                <span className={styles.title}>Brazil.JS Conference 2020</span>
+                <span className={styles.preview}>Hey Luan, I'm confirming my presence in Brazil.JS conference 2020 and I'm hope to meet you soon...</span>
+              </div>
+            </li>
+          </ul>
+        </aside>
+
         <article className={styles.message}>full message mail</article>
       </main>
 
